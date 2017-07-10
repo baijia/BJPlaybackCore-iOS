a08928de4c35b1d1bd5f9af2a686e98a9f245456
辛亚鹏 <xinyapeng@baijiahulian.com>
2017-07-10 11:39:19 +0800

    0.2.10-dylib02

---
+++ BJPlaybackCore/BJPlaybackCore.framework/Versions/A/Headers/BJPlaybackCore.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  Pods
+//
+//  Created by 辛亚鹏 on 2017/1/4.
+//  Copyright (c) 2017 Baijia Cloud. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import "BJPRoom.h"
+
+#endif /* Header_h */
